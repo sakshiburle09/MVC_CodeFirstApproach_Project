40cc1ef515edf45936d309621f06aeee87763c2e
Sakshi Pandurang Burle. <133487425+sakshiburle09@users.noreply.github.com>
2024-10-29 15:01:55 +0530

    Add files via upload

---
+++ Example_Of_Code_First_Approach_MVC/Example_Of_Code_First_Approach_MVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Example_Of_Code_First_Approach_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Example_Of_Code_First_Approach_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Example_Of_Code_First_Approach_MVC/Example_Of_Code_First_Approach_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Example_Of_Code_First_Approach_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
